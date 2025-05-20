85bb60d3115cdff65a3b6359d8aee193b21315a3
richardfn <richard.fogaca@appsilon.com>
2025-05-20 16:55:19 -0300

    clean up the project

--- apps/example/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      'babel-preset-expo',
-      'module:@expo/knex-expo-sqlite-dialect/babel-preset',
-    ],
-  };
-};
